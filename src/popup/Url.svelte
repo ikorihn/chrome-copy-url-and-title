a521e4aef6ce27629e7bb0f4dcbc7ac90b367b85
ikorihn <16367098+ikorihn@users.noreply.github.com>
2022-01-17 00:06:37 +0900

    :sparkles: URL,titleそれぞれコピーボタン追加、ボタン押したらtransitionでメッセージだす

--- src/popup/Url.svelte
+++ src/popup/Url.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
+
   let url: string | null = null
   let title: string | null = null
+
+  let visible: boolean = false
 
   getUrl()
 
@@ -18,6 +22,26 @@
   async function copyUrlAndTitle() {
     try {
       await navigator.clipboard.writeText(`[${title}](${url})`)
+      visible = true
+      setTimeout(() => (visible = false), 1000)
+    } catch (err) {
+      console.error('Failed to copy: ', err)
+    }
+  }
+  async function copyUrl() {
+    try {
+      await navigator.clipboard.writeText(url)
+      visible = true
+      setTimeout(() => (visible = false), 1000)
+    } catch (err) {
+      console.error('Failed to copy: ', err)
+    }
+  }
+  async function copyTitle() {
+    try {
+      await navigator.clipboard.writeText(title)
+      visible = true
+      setTimeout(() => (visible = false), 1000)
     } catch (err) {
       console.error('Failed to copy: ', err)
     }
@@ -26,12 +50,27 @@
 
 <main>
   <section class="title">
-    <h2 class="title__label text-xl font-bold">Title</h2>
+    <h2 class="title__label text-l font-bold">Title</h2>
     <div class="title__content">{title}</div>
   </section>
-  <section class="url">
-    <h2 class="url__label text-xl font-bold">Url</h2>
+  <section class="url mt-2">
+    <h2 class="url__label text-l font-bold">URL</h2>
     <div class="url__content">{url}</div>
   </section>
-  <button class="button w-full rounded bg-blue-400 text-white" on:click={copyUrlAndTitle}>Copy</button>
+  <button
+    class="button mt-2 py-2 w-full rounded bg-blue-400 text-l text-white"
+    on:click={copyUrlAndTitle}>Copy as markdown</button
+  >
+  <button
+    class="button mt-2 py-2 w-full rounded bg-blue-400 text-l text-white"
+    on:click={copyTitle}>Copy title</button
+  >
+  <button
+    class="button mt-2 py-2 w-full rounded bg-blue-400 text-l text-white"
+    on:click={copyUrl}>Copy URL</button
+  >
+
+  {#if visible}
+    <p class="msg none" transition:fade>Copied! {msg}</p>
+  {/if}
 </main>
